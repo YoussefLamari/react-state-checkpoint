ceb01b8c970c8a7a3b49f6615cb3eeeb5d9fe507
harizikhalil <khalilharizi3@gmail.com>
2020-11-14 18:02:04 +0100

    finished react state checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "react-responsive-modal/styles.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Modal } from "react-responsive-modal";
+import ProfilPhoto from "./image/profilPhoto.jpg";
+import Profil from "./Components/Profile/Profil";
+
+class App extends React.Component {
+  state = {
+    Person: {
+      fullName: "khalil harizi",
+      bio:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia turpis in  condimentum sem, per inceptos himenaeos.",
+      imgSrc: ProfilPhoto,
+      profession: "Fullstack js",
+    },
+    show: false,
+    date: new Date(),
+  };
+  componentDidMount() {
+    this.timer = setInterval(() => this.refrechDate(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+  refrechDate() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+  onOpenModal = () => {
+    this.setState({ show: true });
+  };
+  onCloseModal = () => {
+    this.setState({ show: false });
+  };
+  render() {
+    const { show, date } = this.state;
+    return (
+      <React.Fragment>
+        <div className="date-container">
+          <h2>It's : {date.toLocaleTimeString()}</h2>
+        </div>
+        <div className="App">
+          <h1 style={{ color: "#3fb0ac" }}>Welcome to my website</h1>
+          <p>press the button to show the profile </p>
+          <div>
+            <i className="fas fa-level-down-alt"></i>
+          </div>
+
+          <button className="btn" onClick={this.onOpenModal}>
+            Show Profile
+          </button>
+          <Modal open={show} onClose={this.onCloseModal} center>
+            <Profil persone={this.state.Person} />
+          </Modal>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
